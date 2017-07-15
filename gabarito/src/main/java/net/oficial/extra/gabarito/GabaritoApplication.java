8772a8494e3104348c4304b31afceb71bcb125ab
Hudson Martins <p4p4l3gu4x@gmail.com>
2017-07-14 22:56:44 -0300

    Versão Inicial do Projeto.

---
+++ gabarito/src/main/java/net/oficial/extra/gabarito/GabaritoApplication.java
@@ -0,0 +1,12 @@
+package net.oficial.extra.gabarito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabaritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabaritoApplication.class, args);
+	}
+}
